a08968b6d5ad6785e98578d980b6d0a333ba1f28
Russell <shadow2burn@gmail.com>
2018-09-17 17:36:30 +0600

    Site settings are load from database

--- application/core/KN_Controller.php
+++ application/core/KN_Controller.php
@@ -18,10 +18,26 @@ class KN_Controller extends CI_Controller {
 			redirect('login');
 		}
 
+		$this->data['site'] = $this->getData();
+
 		$this->data['add_js'] = [];
 		$this->data['add_css'] = [];
 		$this->data['active_user'] = $this->ion_auth->user()->row();
 
+	}
+
+	private function getData()
+	{
+		$this->load->model('setting');
+		$settings = $this->setting->all();
+		
+		if(empty($settings)) return $settings; 
+		
+		foreach($settings as $setting){
+			$data[$setting['key']] = $setting['value'];
+		}
+
+		return $data;
 	}
 
 	
--- application/views/layout/default.php
+++ application/views/layout/default.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>KinbeNaki.com | Administrator</title>
+  <title><?=$site['site_title']?> | Administrator</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
@@ -46,7 +46,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>K</b>N<b>C</b></span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>KinbeNaki</b>.com</span>
+        <span class="logo-lg"><b><?=$site['site_title']?></b></span>
       </a>
 
       <!-- Header Navbar: style can be found in header.less -->
